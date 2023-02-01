9e4874a4022e9f43dc3cf4db41ba430b92b9d121
Gabriele Di Cosmo <gabrieledicosmo05@gmail.com>
2023-02-01 16:50:06 +0100

    fine es 3

--- es3.js
+++ es3.js
@@ -1,10 +1,16 @@
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let n = array.length
+let n = array.length;
+let minimo = array[0];
 
 function min(array, n) {
-    if (n===0) {
-        
-    } else {
-        
-    }
+	if (n === 0) {
+		return minimo;
+	} else {
+		if (array[n] < minimo) {
+			minimo = array[n];
+		}
+		return min(array, n - 1);
+	}
 }
+
+console.log(min(array, n - 1));
